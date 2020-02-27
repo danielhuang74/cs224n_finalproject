bebb312aada280b13ffabc41fb25fc0dfa6e9dc4
danielhuang74 <danielhuang74@gmail.com>
2020-02-26 20:19:30 -0800

    added file to open SummaryWriter output

---
+++ transformers/examples/open_summary.py
@@ -0,0 +1,8 @@
+from tensorboard.backend.event_processing.event_accumulator import EventAccumulator
+event_acc = EventAccumulator('runs/Feb25_10-59-10_cs224n-finalproject-nv6-new/events.out.tfevents.1582628350.cs224n-finalproject-nv6-new.4660.0')
+event_acc.Reload()
+# Show all tags in the log file
+print(event_acc.Tags())
+
+# E. g. get wall clock, number of steps and value for a scalar 'Accuracy'
+w_times, step_nums, vals = zip(*event_acc.Scalars('Accuracy'))
