26820a220217bfa4ed753a740d6d9dd58d255c7a
Shan Zhou <shanzhou@fb.com>
2020-03-05 21:51:46 -0800

    add generate weights file in run_squd

--- transformers/examples/run_squad.py
+++ transformers/examples/run_squad.py
@@ -91,6 +91,34 @@ MODEL_CLASSES = {
 }
 
 
+
+from scipy.stats import truncnorm
+def truncated_normal(size, threshold=1):
+    values = truncnorm.rvs(-threshold, threshold, size=size)
+    x = torch.from_numpy(values)
+    return x
+
+def reinitialize_weights():
+    REINITIALIZE_WEIGHTS_FILE = "weights/reinitialize_weights_layer10_layer11.bin"
+    pretrain_file="weights/bert-base-uncased-pytorch_model.bin"
+    pretrain_state_dict = torch.load(pretrain_file, map_location="cpu")
+    
+    state_dict = {}
+    for k in pretrain_state_dict.keys():
+        if '.10.' in k or '.11.' in k:
+            print('REINITIALIZED: ', k)
+            parameters = truncated_normal(pretrain_state_dict[k].shape)
+            state_dict[k] = parameters
+        else:
+            print('PRETAIN: ', k)
+            state_dict[k] = pretrain_state_dict[k]
+        
+    torch.save(state_dict, REINITIALIZE_WEIGHTS_FILE)
+    print("saved reinitialized weights: ", REINITIALIZE_WEIGHTS_FILE)
+    
+    return REINITIALIZE_WEIGHTS_FILE
+
+
 def set_seed(args):
     random.seed(args.seed)
     np.random.seed(args.seed)
@@ -769,8 +797,11 @@ def main():
         cache_dir=args.cache_dir if args.cache_dir else None,
     )
     
-    state_dict = torch.load('reinitialize_weights_layer0_layer1.bin', map_location="cpu")
-    print("-----------------------------loaded random generarized weights for layer 0 and 1 ----------------")
+    
+    reinitialize_weight_file = reinitialize_weights()
+    state_dict = torch.load(reinitialize_weight_file, map_location="cpu")
+    
+    print("-----------------------------loaded random generarized weights from %s ----------------"%reinitialize_weight_file)
     model = model_class.from_pretrained(
         args.model_name_or_path,
         from_tf=bool(".ckpt" in args.model_name_or_path),
@@ -778,7 +809,7 @@ def main():
         cache_dir=args.cache_dir if args.cache_dir else None,
         state_dict=state_dict,
     )
-    print("----------------load model with random generarized weights for layer 0 and 1---------------------")
+    print("----------------load model with random generarized weights from %s ---------------------"%reinitialize_weight_file)
     if args.local_rank == 0:
         # Make sure only the first process in distributed training will download model & vocab
         torch.distributed.barrier()
