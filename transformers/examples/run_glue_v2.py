e7391aa4b7788beebe8b9db6605d98978aba6e93
Shan Zhou <shanzhou@fb.com>
2020-03-12 22:32:59 -0700

    run only layer 7

--- transformers/examples/run_glue_v2.py
+++ transformers/examples/run_glue_v2.py
@@ -789,7 +789,8 @@ if __name__ == "__main__":
     #     weight_dict[key] = reinitialize_weight_file
 
     weight_dict = OrderedDict()
-    for i in range(7,8):
+    # for i in range(7,8):
+    for i in [0,4,8,11]:
         key = 'layer%s'%str(i)
         reinitialize_weight_file = "weights/reinitialize_weights_%s.bin"%key
         weight_dict[key] = reinitialize_weight_file
