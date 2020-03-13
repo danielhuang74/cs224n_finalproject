458d4ccbd44c297ce6ead93a3cd1d15a60627b0e
Jacky Lin <jackylin@stanford.edu>
2020-03-13 15:37:30 -0700

    update plotting

--- transformers/examples/open_summary.py
+++ transformers/examples/open_summary.py
@@ -1,10 +1,50 @@
 from tensorboard.backend.event_processing.event_accumulator import EventAccumulator
-event_acc = EventAccumulator('runs/Feb25_10-59-10_cs224n-finalproject-nv6-new/events.out.tfevents.1582628350.cs224n-finalproject-nv6-new.4660.0')
-event_acc.Reload()
-# Show all tags in the log file
-print(event_acc.Tags())
+# STSB_on_CoLA = EventAccumulator('runs/STS-B_on_CoLA/events.out.tfevents.1584081577.nv6-2.113031.0')
+# STSB_on_MRPC = EventAccumulator('runs/STS-B_on_MRPC')
 
-print(event_acc.Scalars('loss'))
+# STSB_on_CoLA = EventAccumulator('runs/Feb27_19-33-45_cs224n-finalproject-nv6-new/events.out.tfevents.1582832025.cs224n-finalproject-nv6-new.74808.0')
+# STSB_on_CoLA = EventAccumulator('runs/STS-B_baseline/events.out.tfevents.1584082439.nv6-2.117454.0')
+
+STSB_on_CoLA = EventAccumulator('runs/STS-B_baseline_test/events.out.tfevents.1584087778.nv6-2.12277.0')
+
+
+
+
+STSB_on_CoLA.Reload()
+# STSB_on_MRPC.Reload()
+# Show all tags in the log file
+# print(STSB_on_CoLA)
+# print()
+
+# print(STSB_on_CoLA.Scalars('Accuracy'))
+# print(STSB_on_CoLA.Scalars("loss"))
+
+
+import pandas as pd
+from datetime import datetime, timedelta, date
+from collections import defaultdict
+import math
+import matplotlib
+import numpy as np
+import matplotlib.pyplot as plt
+import numpy as np
+import time
+import math
+from tqdm import tqdm
+from numpy import genfromtxt
+import csv
+from collections import defaultdict
+
+
+steps = []
+values = []
+for wall_time, step, value in STSB_on_CoLA.Scalars("loss"):
+    steps.append(step)
+    values.append(value)
+
+plt.plot(steps, values)
+plt.show()
+
 
 # E. g. get wall clock, number of steps and value for a scalar 'Accuracy'
-#w_times, step_nums, vals = zip(*event_acc.Scalars('Accuracy'))
+#w_times, step_nums, vals = zip(*event_acc.Scalars('Accuracy'
